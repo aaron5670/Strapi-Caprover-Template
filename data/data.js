3654622be2285d662f988317a95a3124e063bc07
Aaron van den Berg <a.vdberg98@gmail.com>
2021-10-08 12:30:48 +0200

    Clean up template + database

--- data/data.js
+++ data/data.js
@@ -1,9 +1,8 @@
 const { global, pages } = require("./en");
-const { globalFR, pagesFR } = require("./fr");
 const { leadFormSubmissions } = require("./lead-form-submissions.json");
 
 module.exports = {
-  globals: [global, globalFR],
-  pages: [...pages, ...pagesFR],
+  globals: [global],
+  pages: [...pages],
   leadFormSubmissions,
 };
